69c2f9b3a6905bc78de4303d8bdafd7901d04de3
Federico Harjes Ruiloba <fedeharjes@gmail.com>
2018-04-19 22:56:27 -0400

    Added textbox and size slider

--- sketch.js
+++ sketch.js
@@ -11,12 +11,18 @@ let sliderRed;
 let sliderGreen;
 let sliderBlue;
 
-let sliderY = 280;
+let sliderY = 270;
 let sliderSpacing = 146;
 
 let rColor;
 let gColor;
 let bColor;
+
+let sliderSize;
+
+let textbox;
+let input;
+let inputInfo;
 
 canvasWidth = 400;
 canvasHeight = 300;
@@ -37,7 +43,13 @@ function setup() {
     pRed = createP('Red color');
     pGreen = createP('Green color');
     pBlue = createP('Blue color');
-} 
+
+    sliderSize = createSlider(7, 70, 25);
+
+    textbox = createInput('NO');
+
+    inputInfo = createP('*if you want a crazy-colored point, type yes and hit the return key. To stop the effect simply change your input.')
+}
 
 function draw() {
     background(240);
@@ -45,21 +57,21 @@ function draw() {
 
     fill(rColor, gColor, bColor);
 
-    ellipse(x, y, 25);
+    ellipse(x, y, sliderSize.value());
 
     button1.position(76, 615);
     button2.position(276, 615)
 
-    sliderRed.position(470, sliderY - 10); 
-    sliderGreen.position(470, sliderY + sliderSpacing - 10); 
-    sliderBlue.position(470, sliderY + sliderSpacing * 2 -10); 
+    sliderRed.position(470, sliderY);
+    sliderGreen.position(470, sliderY + sliderSpacing);
+    sliderBlue.position(470, sliderY + sliderSpacing * 2);
 
-    pRed.position(493, sliderY);
-    pGreen.position(485, sliderY + sliderSpacing);
-    pBlue.position(492, sliderY + sliderSpacing * 2);
+    pRed.position(493, sliderY + 7);
+    pGreen.position(485, sliderY + 7 + sliderSpacing);
+    pBlue.position(492, sliderY + 7 + sliderSpacing * 2);
 
-    x = noise(xoff)*300;
-    y = noise(yoff)*200;
+    x = noise(xoff) * 300;
+    y = noise(yoff) * 200;
 
     xoff += speed;
     yoff += speed;
@@ -72,6 +84,27 @@ function draw() {
     gColor = sliderGreen.value();
     bColor = sliderBlue.value();
 
+    sliderSize.position(570, 415);
+    sliderSize.style('transform', 'rotateZ(270deg)');
+    sliderSize.style('width', '300px')
+
+    let haveFun = select('#p1');
+    haveFun.style('color', '#FF4B7D');
+
+    let randomize = select('#randomize');
+    randomize.position(30, 660);
+
+    textbox.position(170, 685);
+    textbox.style('width', '30px')
+   
+    input = textbox.value();
+
+    if (input == 'yes' && keyCode === RETURN) {
+        randomizePoint();
+    } 
+
+    inputInfo.position(30, 710);
+    inputInfo.style('font-size', '14pt');
 }
 
 function faster() {
@@ -81,4 +114,10 @@ function faster() {
 
 function slower() {
     speed -= 0.003;
+}
+
+function randomizePoint() {
+    rColor = random(0, 255);
+    gColor = random(0, 255);
+    bColor = random(0, 255);
 }
